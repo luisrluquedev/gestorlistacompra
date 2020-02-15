fc3865ac8000c5a3e49b8d5f785319b44deb163b
Luis <lluisrl@outlook.com>
2020-02-15 13:27:51 +0100

    Tarea Servicio creada

---
+++ src/app/services/tarea.service.ts.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Tarea } from '../models/compraslist';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TareaServiceTsService {
+  tareas: Tarea[] = [
+    { id: 1, titulo: 'Leche', cantidad: 6 },
+    { id: 2, titulo: 'Pan', cantidad, 1},
+    { id: 3, titulo: 'Pasta dientes', cantidad: 2 },
+  ];
+
+  constructor() { }
+
+  getTareas() {
+    return this.tareas;
+  }
+}
