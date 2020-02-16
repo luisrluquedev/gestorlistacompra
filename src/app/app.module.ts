dfcda07583e299b8d92a0f3f111609459e21d6b0
Luis <lluisrl@outlook.com>
2020-02-16 17:02:33 +0100

    Importación FormsModule y ReactiveFormsModule - preguntar diferencia...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TareaServiceTsService } from "./services/tarea.service.ts.service";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [TareaServiceTsService],
   bootstrap: [AppComponent]
